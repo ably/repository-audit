1eb1fc2e37f231dbf8c9485f1e988a8c747f3fe4
Quintin Willison <q@qwuk.net>
2021-10-06 10:27:36 +0100

    Add commentary to strict equality check functions.
    This is an attempt to explain why they're there, in response to comment on my pull request.

--- strict.js
+++ strict.js
@@ -1,4 +1,17 @@
+/**
+ * Uses a strict equality check (`===`) to validate that `value` is `false`.
+ *
+ * @param {*} value The value to be checked.
+ * @returns {boolean} Only returns `true` if `value` is of 'boolean' type with value `false` (strict equality check).
+ */
 const isFalse = (value) => value === false;
+
+/**
+ * Uses a strict equality check (`===`) to validate that `value` is `true`.
+ *
+ * @param {*} value The value to be checked.
+ * @returns {boolean} Only returns `true` if `value` is of 'boolean' type with value `true` (strict equality check).
+ */
 const isTrue = (value) => value === true;
 
 module.exports = {
