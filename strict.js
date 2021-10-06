dfcfbb1be64aed792b8a4822bb578711ade8b382
Quintin Willison <q@qwuk.net>
2021-10-06 16:11:50 +0100

    Change strict module behaviour to throw error on encountering unexpected type.

--- strict.js
+++ strict.js
@@ -1,18 +1,29 @@
+const isBoolean = (value, expectedValue) => {
+  const typeOfValue = typeof value;
+  const expectedType = 'boolean';
+  if (typeOfValue !== expectedType) {
+    throw new Error(`Unexpected type '${typeOfValue}' when expected '${expectedType}.`);
+  }
+  return value === expectedValue;
+};
+
 /**
  * Uses a strict equality check (`===`) to validate that `value` is `false`.
+ * If the type of `value` is not `boolean` then this function throws an error.
  *
  * @param {*} value The value to be checked.
  * @returns {boolean} Only returns `true` if `value` is of 'boolean' type with value `false` (strict equality check).
  */
-const isFalse = (value) => value === false;
+const isFalse = (value) => isBoolean(value, false);
 
 /**
  * Uses a strict equality check (`===`) to validate that `value` is `true`.
+ * If the type of `value` is not `boolean` then this function throws an error.
  *
  * @param {*} value The value to be checked.
  * @returns {boolean} Only returns `true` if `value` is of 'boolean' type with value `true` (strict equality check).
  */
-const isTrue = (value) => value === true;
+const isTrue = (value) => isBoolean(value, true);
 
 module.exports = {
   isFalse,
